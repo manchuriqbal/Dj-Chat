320cd8d0502db38095e657f3f0718582d7c6e1d1
manchuriqbal <manchur.fiverr@gmail.com>
2023-10-02 18:50:04 +0600

    complete signup

---
+++ core/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+from .forms import SignupForm
+
+def frontpage(request):
+    return render(request, 'core/frontpage.html')
+
+
+def signup(request):
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+            return redirect('frontpage')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {'form' : form})
