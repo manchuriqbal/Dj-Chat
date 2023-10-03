69bc26ba86aa7449e13050040e22d470ee158aa1
manchuriqbal <manchur.fiverr@gmail.com>
2023-10-03 07:49:40 +0600

    logout fucntionality

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_view
 from django.urls import path
 
 from . import views
@@ -5,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.frontpage, name="frontpage"),
     path('signup/', views.signup, name="signup"),
+    path('logout/', auth_view.LogoutView.as_view(), name="logout"),
 ]
